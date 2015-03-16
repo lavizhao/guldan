265446446d371427a6e1432bee32531639274b85
lavi <zhaoximo@hotmail.com>
2015-03-17 07:59:16 +0800

    heh

--- sh/ctr.sh
+++ sh/ctr.sh
@@ -8,7 +8,7 @@ echo 编译文件
 g++ ftrl.cpp -O3
 
 echo 跑CTR
-./a.out -train ../../CTR/data/new_train.csv.hash -test ../../CTR/data/new_train.csv.hash -out ../data/out.csv -l2 1 -l1 0 -iter 10 -alpha 0.1 -beta 1 -verbose 200 -stop 2
+./a.out -train ../../CTR/data/new_train.csv.hash -test ../../CTR/data/new_train.csv.hash -out ../data/out.csv -l2 1 -l1 1 -iter 40 -alpha 0.1 -beta 1 -verbose 200 -stop 0.3
 
 echo 测AUC
 cd ../script
